3808aac14c3492f431134264ddc3d4ad7285e82d
Tyler Noble <tylersnoble@gmail.com>
2020-02-04 18:47:02 -0500

    Added Burgers and ingredients

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Ax from '../../hoc/Ax';
+import classes from './Layout.css'
 const layout = (props) => (
    <Ax>
     <div> Toolbar,Sidebar, Backdrop</div>
-    <main>
+    <main className={classes.Content}>
        {props.children} 
     </main>
     </Ax>
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,13 @@
+import React,{ Component } from "react";
+import Ax from '../../hoc/Ax'
+class BurgerBuilder extends Component{
+    render(){
+        return(
+            <Ax>
+                <div>Burger</div>
+            <div>Build Controls</div>
+            </Ax>
+        );
+    }
+}
+export default BurgerBuilder;

8d5a8460e466aa7026509626e92f766561192af6
Tyler Noble <tylersnoble@gmail.com>
2020-02-06 17:10:40 -0500

    It now dynamically allocates burger compenents
    it uses an array with a lot of mapping

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,22 @@ import React,{ Component } from "react";
 import Ax from '../../hoc/Ax'
 import Burger from '../../components/Burger/Burger'
 class BurgerBuilder extends Component{
+   /* constructor(props){
+        super(props);
+        this.state = {...}
+   }*/
+   state = {
+       ingredients:{
+           salad:1,
+           bacon:1,
+           cheese:2,
+           meat:2
+       }
+   }
     render(){
         return(
             <Ax>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
             <div>Build Controls</div>
             </Ax>
         );

b34814430a98d79198c0d51c8bab306e8da24e02
Tyler Noble <tylersnoble@gmail.com>
2020-02-08 16:48:35 -0500

    Adding ingredients now works.
    You just need to pass a bunch of values through props then alter the state. How simple!

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,13 @@
 import React,{ Component } from "react";
-import Ax from '../../hoc/Ax'
-import Burger from '../../components/Burger/Burger'
+import Ax from '../../hoc/Ax';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+const INGREDIENT_PRICES = {
+    salad:0.5,
+    cheese:0.1,
+    bacon:0.7,
+    meat: 0.3
+}
 class BurgerBuilder extends Component{
    /* constructor(props){
         super(props);
@@ -12,13 +19,29 @@ class BurgerBuilder extends Component{
            bacon:0,
            cheese:0,
            meat:0
-       }
+       },
+       totalPrice: 10
+   }
+   addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount+ 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice+ priceAddition;
+        const newPrice = oldPrice+priceAddition;
+        this.setState({totalPrice:newPrice,ingredients:updatedIngredients});
+   }
+   removeIngredientHandler = (type) => {
+
    }
     render(){
         return(
             <Ax>
                 <Burger ingredients={this.state.ingredients}/>
-            <div>Build Controls</div>
+                <BuildControls ingredientAdded={this.addIngredientHandler} />
             </Ax>
         );
     }

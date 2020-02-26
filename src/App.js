e2222d7ba8e0eee8c2ec972415f9ed9257afb932
Tyler Noble <tylersnoble@gmail.com>
2020-02-26 13:43:40 -0500

    Added router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './index.css'
 import Layout from './components/Layout/Layout'
 import Checkout from './containers/Checkout/Checkout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import { Route, Switch } from 'react-router-dom'
 class App extends Component {
   state = {
     show: true
@@ -17,8 +18,10 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout/>
+          <Switch>
+            <Route path='/checkout' component={Checkout} />
+            <Route path='/' exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     )

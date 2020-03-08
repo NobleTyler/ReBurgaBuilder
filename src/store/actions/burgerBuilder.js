884f960e864b3725ff23a7fe87af67b4cdc6d1fc
Tyler Noble <tylersnoble@gmail.com>
2020-03-08 02:44:50 -0400

    Added redux-thunk

---
+++ src/store/actions/burgerBuilder.js
@@ -0,0 +1,14 @@
+import * as actionTypes from './actionTypes'
+
+export const addIngredient =(name) =>{
+    return{
+        ingredientName:name,
+        type: actionTypes.ADD_INGREDIENT
+    }
+}
+export const removeIngredient =(name) =>{
+    return{
+        ingredientName:name,
+        type: actionTypes.REMOVE_INGREDIENT
+    }
+} 

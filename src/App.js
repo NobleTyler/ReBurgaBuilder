f4dd4d73072ae23fe239c5d5a67b8773dcb23863
Tyler Noble <tylersnoble@gmail.com>
2020-03-10 12:29:24 -0400

    Added Auth form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Checkout from './containers/Checkout/Checkout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import { Route, Switch } from 'react-router-dom'
 import Orders from './containers/Orders/Orders'
+import Auth from './containers/Auth/Auth'
 class App extends Component {
   state = {
     show: true
@@ -22,6 +23,7 @@ class App extends Component {
           <Switch>
             <Route path='/checkout' component={Checkout} />
             <Route path='/orders' component={Orders} />
+            <Route path='/auth' component={Auth} />
             <Route path='/' exact component={BurgerBuilder} />
           </Switch>
         </Layout>

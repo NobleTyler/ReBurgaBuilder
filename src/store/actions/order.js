7d619f518a752b68a6b457d8c6420d0787a155a1
Tyler Noble <tylersnoble@gmail.com>
2020-03-11 14:45:15 -0400

    Passes Auth token around
    And due to firebase changes you need auth to see orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -20,11 +20,11 @@ export const purchaseBurgerStart = () => {
         type: actionTypes.PURCHASE_BURGER_START
     }
 }
-export const purchaseBurger = (orderData) => {
+export const purchaseBurger = (orderData,token) => {
     return dispatch => {
         dispatch(purchaseBurgerStart())
         axios
-            .post('/orders.json', orderData)
+            .post('/orders.json?auth='+token , orderData)
             .then(response => {
                 dispatch(purchaseBurgerSuccess(response.data.name, orderData))
             })
@@ -56,11 +56,11 @@ export const fetchOrdersStart = () => {
         type: actionTypes.FETCH_ORDERS_START
     }
 }
-export const fetchOrders = () => {
+export const fetchOrders = (token) => {
     return dispatch => {
         dispatch(fetchOrdersStart())
         axios
-            .get('/orders.json')
+            .get('/orders.json?auth='+token)
             .then(res => {
                 const fetechedOrders = []
                 for (let key in res.data) {

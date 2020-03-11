12fb3ad424323f95b516d37f06f2da875f17847a
Tyler Noble <tylersnoble@gmail.com>
2020-03-11 15:05:52 -0400

    Logout now sends you to home page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import { Route, Switch } from 'react-router-dom'
 import Orders from './containers/Orders/Orders'
 import Auth from './containers/Auth/Auth'
+import Logout from './containers/Auth/Logout/Logout'
 class App extends Component {
   state = {
     show: true
@@ -24,6 +25,7 @@ class App extends Component {
             <Route path='/checkout' component={Checkout} />
             <Route path='/orders' component={Orders} />
             <Route path='/auth' component={Auth} />
+            <Route path='/logout' component={Logout} />
             <Route path='/' exact component={BurgerBuilder} />
           </Switch>
         </Layout>

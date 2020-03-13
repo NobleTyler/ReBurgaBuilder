c5ec7c0c74ae276fd4a4c7c140b187bc36db5954
Tyler Noble <tylersnoble@gmail.com>
2020-03-13 19:14:03 -0400

    Begin adding tests

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItems.test.js
@@ -0,0 +1,3 @@
+describe('<NavigationItems />', () =>{
+    it("should render two navigation items if not signed in")
+})
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import classes from '../NavigationItems/NavigationItems.css'
 import NavigationItem from '../NavigationItems/NavigationItem/NavigationItem'
+
 const navigationItems = props => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem link='/' exact >Burger Builder</NavigationItem>
-    {props.isAuthenticated? 
-    <NavigationItem link='/orders'>Orders</NavigationItem>:
-    null}
-    {props.isAuthenticated? 
-    <NavigationItem link='/logout'>Logout</NavigationItem>:
-    <NavigationItem link='/auth'>Sign Up/In</NavigationItem>}
+    <NavigationItem link='/' exact>
+      Burger Builder
+    </NavigationItem>
+    {props.isAuthenticated ? (
+      <NavigationItem link='/orders'>Orders</NavigationItem>
+    ) : null}
+    {props.isAuthenticated ? (
+      <NavigationItem link='/logout'>Logout</NavigationItem>
+    ) : (
+      <NavigationItem link='/auth'>Sign Up/In</NavigationItem>
+    )}
   </ul>
 )
 export default navigationItems

42ad73de03c2df244592fbaf0816be0634fa24b0
Tyler Noble <tylersnoble@gmail.com>
2020-03-13 15:04:08 -0400

    Added lazy loading

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import './index.css'
 import * as actions from './store/actions/index'
 import {connect} from 'react-redux'
 import Layout from './components/Layout/Layout'
-import Checkout from './containers/Checkout/Checkout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import { Route, Switch,withRouter,Redirect } from 'react-router-dom'
-import Orders from './containers/Orders/Orders'
-import Auth from './containers/Auth/Auth'
 import Logout from './containers/Auth/Logout/Logout'
+import asyncComponent from './hoc/asyncComponent/asyncComponent'
+
+//Lazy Loading Modules
+const asyncCheckout = asyncComponent(()=>{
+  return import('./containers/Checkout/Checkout')
+})
+const asyncOrders = asyncComponent(()=>{
+  return import('./containers/Orders/Orders')
+})
+const asyncAuth = asyncComponent(()=>{
+  return import('./containers/Auth/Auth')
+})
+
 class App extends Component {
   componentDidMount () {
   this.props.onTryAutoSignup()
@@ -16,16 +26,16 @@ class App extends Component {
   render () {
     let routes = (
       <Switch>
-            <Route path='/auth' component={Auth} />
+            <Route path='/auth' component={asyncAuth} />
             <Route path='/' exact component={BurgerBuilder} />
             <Redirect to='/'/>
       </Switch>
     )
     if(this.props.isAuthenticated){
       routes =(<Switch>
-        <Route path='/checkout' component={Checkout} />
-            <Route path='/orders' component={Orders} />
-            <Route path='/auth' component={Auth} />
+        <Route path='/checkout' component={asyncCheckout} />
+            <Route path='/orders' component={asyncOrders} />
+            <Route path='/auth' component={asyncAuth} />
             <Route path='/logout' component={Logout} />
             <Route path='/' exact component={BurgerBuilder} />
             <Redirect to='/'/>

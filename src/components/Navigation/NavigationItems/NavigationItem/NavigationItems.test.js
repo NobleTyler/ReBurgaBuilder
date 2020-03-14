82408b1115caf105e7a8548a784c88d85f27c6a1
NobleTyler <tynobledev@gmail.com>
2020-03-13 21:05:04 -0400

    Added some basic tests to Navigation Item
    Should do more later

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItems.test.js
+++
@@ -1,3 +0,0 @@
-describe('<NavigationItems />', () =>{
-    it("should render two navigation items if not signed in")
-})
---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,23 @@
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import NavigationItems from './NavigationItems'
+import NavigationItem from './NavigationItem/NavigationItem'
+import React from 'react'
+
+configure({ adapter: new Adapter() })
+
+describe('<NavigationItems />', () => {
+    let wrapper
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />)
+    })
+    it("should render two navigation items if not signed in", () => {
+        expect(wrapper.find(NavigationItem)).toHaveLength(2)
+    })
+    it("should render two navigation items if signed in", () => {
+        wrapper.setProps({
+            isAuthenticated: true
+        })
+        expect(wrapper.find(NavigationItem)).toHaveLength(3)
+    })
+})

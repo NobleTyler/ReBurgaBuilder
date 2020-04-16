30406ca60b05e480954b9d136ba8136ed77d1f8d
Tyler Noble <tylersnoble@gmail.com>
2020-04-16 17:14:43 -0400

    Commented all HOC's
    Higher order components are now commented

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -2,6 +2,13 @@ import React from 'react'
 import useHttpErrorHandler from '../../hooks/http-error-handler'
 import Modal from '../../components/UI/Modal/Modal'
 
+/**
+ * Used to wrap anything we want eror handling on
+ * It hanldes http errors from axios
+ * And shows a modal if things fail
+ * @param {*} WrappedComponent
+ * @param {*} axios
+ */
 const withErrorHandler = (WrappedComponent, axios) => {
   return props => {
     const [error, clearError] = useHttpErrorHandler(axios)
